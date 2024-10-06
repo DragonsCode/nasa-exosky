68e6346bd3e70e6953dc1b272fe48d90a6bb0b83
DragonsCode <dilimmurod@gmail.com>
2024-10-06 11:16:01 +0500

    Fixes

--- exosky/exosky/settings.py
+++ exosky/exosky/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&noj91mt%#svz@z55n4k4=zal2wsgs+4ba^w7%nrn9a$%jdpm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
